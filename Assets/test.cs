c3f91515a62033a8b9b523e893c5595c08292ad0
vedi <learnoverandabove@gmail.com>
2020-04-23 21:14:13 +0530

    committed sample

---
+++ Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string []texts = {"1","2","3","4"};
+    void Start()
+    {
+        
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < texts.Length; t++ )
+        {
+            string tmp = texts[t];
+            int r = Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	Debug.Log(texts[0]);   
+    }
+}
---
+++ Assets/test1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string []texts = {"1","2","3","4"};
+    void Start()
+    {
+        
+       
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < texts.Length; t++ )
+        {
+            string tmp = texts[t];
+            int r = Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
+        }
+     	Debug.Log(texts[0]);   
+    }
+}
